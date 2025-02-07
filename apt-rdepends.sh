783c1b6f6af635f7f31ec62d3a8e0e369f58771a
jay133668 <66983345+jay133668@users.noreply.github.com>
2025-02-07 14:53:53 +0800

    Update apt-rdepends.sh

--- apt-rdepends.sh
+++ apt-rdepends.sh
@@ -1,1 +1,23 @@
 
+#!/bin/bash
+
+#$1     pkg
+get_all_depends()
+{
+        apt-rdepends $1 | grep -v "^ "
+}
+
+
+## 遍历命令行参数，参数应为包名。
+for pkg in get_all_depends
+do
+        echo -e "所有依赖共计"$(get_all_depends | wc -w)"个"
+        get_all_depends
+        i=0
+        for depend in get_all_depends
+        do
+                i=$((i+1))
+                echo -e "\033[1;32m正在下载第$i个依赖："$depend "\033[0m"
+                apt-get download $depend
+        done
+done
