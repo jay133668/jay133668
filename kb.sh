279fd461afec5ace471ce88f03c150698623e214
jay133668 <66983345+jay133668@users.noreply.github.com>
2025-03-20 14:27:53 +0800

    Update kb.sh

--- kb.sh
+++ kb.sh
@@ -3,3 +3,9 @@
 remove_rc_pkg () {
 dpkg -l | grep ^rc | cut -d' ' -f3 | sudo xargs dpkg --purge
 }
+
+
+#如何去除UNIX系統下文件中的换行符^M ：
+Linux: dos2unix filename
+Aix :cat filename | perl -pe '~s/\r//g' > filename
+或者cat filename | tr -d "\r" > filename
