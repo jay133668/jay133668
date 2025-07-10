ccc64463925f8c104ccb66b6eb034f9ff9ce9e21
jay133668 <66983345+jay133668@users.noreply.github.com>
2025-07-10 10:52:29 +0800

    Update aix_kb.sh

--- aix_kb.sh
+++ aix_kb.sh
@@ -1,3 +1,27 @@
 #aix vio 進入 root：
 oem_setup_env
 
+# 獲取power ha info:
+#vios:
+oem_setup_env
+[ ! -d /tmp/to-ibm ] && mkdir /tmp/to-ibm || echo "folder already exsit"
+lspv >> /tmp/to-ibm/script-`hostname`.txt
+lsdev -Cc disk >> /tmp/to-ibm/script-`hostname`.txt
+gzip /tmp/to-ibm/script-`hostname`.txt
+chmod -R 777 /tmp/to-ibm
+du -sk /tmp/to-ibm/script-`hostname`.txt.gz
+scp /tmp/to-ibm/script-`hostname`.txt.gz emts02@10.122.5.119:/tmp/to-ibm/
+
+#aix:
+[ ! -d /tmp/to-ibm ] && mkdir /tmp/to-ibm || echo "folder already exsit"
+lspv >> /tmp/to-ibm/script-`hostname`.txt
+lsdev -Cc disk >> /tmp/to-ibm/script-`hostname`.txt
+cd /usr/es/sbin/cluster/utilities/ 
+cldisp >> /tmp/to-ibm/script-`hostname`.txt
+cltopinfo >> /tmp/to-ibm/script-`hostname`.txt
+clshowres >> /tmp/to-ibm/script-`hostname`.txt
+clmgr verify cluster &>> /tmp/to-ibm/script-`hostname`.txt
+gzip /tmp/to-ibm/script-`hostname`.txt
+chmod -R 777 /tmp/to-ibm
+du -sk /tmp/to-ibm/script-`hostname`.txt.gz
+
