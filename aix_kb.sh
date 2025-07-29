b1faca147691f0695d2e5ce6b759069824a6c2fe
jay133668 <66983345+jay133668@users.noreply.github.com>
2025-07-29 09:53:52 +0800

    Update aix_kb.sh

--- aix_kb.sh
+++ aix_kb.sh
@@ -25,3 +25,37 @@ gzip /tmp/to-ibm/script-`hostname`.txt
 chmod -R 777 /tmp/to-ibm
 du -sk /tmp/to-ibm/script-`hostname`.txt.gz
 
+################################################################################################
+################################################################################################
+#nfs server:
+hbat=`hostname | cut -c1-2`
+echo "/tmp/os_patch   -sec=sys,rw,access=${hbat}vapp01:${hbat}2bat01:${hbat}vdb001:${hbat}app02:${hbat}vdb001:${hbat}ebkdb01,root=${hbat}2bat01" >> /etc/exports
+exportfs -v
+exportfs -a
+exportfs -v
+################################################################################################
+#nfs client:
+[ ! -d /tmp/os_patch ] && mkdir -p /tmp/os_patch && chmod -R 777 /tmp/os_patch
+hbat=`hostname | cut -c1-2`
+dev_share_ip=10.122.43.115
+sdc_share_ip=10.122.20.119
+pdc_share_ip=10.122.5.119
+
+case $hbat in
+	de) export sip=$dev_share_ip ;;
+	sd)	export sip=$sdc_share_ip ;;
+	pd)	export sip=$pdc_share_ip ;;
+esac
+
+echo "/tmp/os_patch:
+        dev             = "/tmp/os_patch"
+        vfs             = nfs
+        nodename        = $sip
+        mount           = true
+        options         = rw,bg,hard,intr,sec=sys
+        account         = false" >> /etc/filesystems
+
+mount /tmp/os_patch
+
+lsfs -l /tmp/os_patch
+ll /tmp/os_patch
