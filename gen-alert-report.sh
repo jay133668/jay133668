79423d6733cbb3b432a9503d92567a06cd1b8a35
jay133668 <66983345+jay133668@users.noreply.github.com>
2025-09-09 17:58:31 +0800

    Update gen-alert-report.sh

--- gen-alert-report.sh
+++ gen-alert-report.sh
@@ -1,4 +1,4 @@
-
+#!/bin/bash
 ###################################################################
 emf=`date +%B`
 curyear=`date +%Y`
@@ -11,26 +11,28 @@ acve=/tmp/get_alerts/info/all_cvenum.msg
 alink=/tmp/get_alerts/info/link.msg
 aan=/tmp/get_alerts/info/all_alert_num.msg
 curl -s $hardlink | egrep -B15 "$mon" | egrep "pubDate|title|guid" > $msg_file
-egrep title $msg_file | cut -d ":" -f2  | sed "s#</title>##g" > $atf
-egrep title $msg_file | egrep -o "[a-Z][0-9][0-9]-$curmon-[0-9][0-9]" > $acve
 egrep guid $msg_file | sed  "s/<guid>//g" | sed  "s#</guid>##g" | awk '{print $1}' > $alink
-egrep guid $msg_file | egrep -o "[0-9]{4,}" > $aan
+
+#egrep title $msg_file | cut -d ":" -f2  | sed "s#</title>##g" > $atf
+#egrep title $msg_file | egrep -o "[a-Z][0-9][0-9]-$curmon-[0-9][0-9]" > $acve
+#cat $alink | egrep -o "[0-9]{4,}" > $aan
 #############################################
 for link in `cat $alink`
 do
-tp=/tmp/get_alerts/info
-nf=/$tp/$num-all.msg
-cnf=$tp/$num-cve.msg
-descf=$tp/$num-desc.msg
-detailf=$tp/$num-detailf.msg
-link=${link%$'\r'}
-curl -s $link -o /tmp/get_alerts/info/$num-all.msg
 alnum=`echo $link  | egrep -o "[0-9]{4,}"` 
+tp=/tmp/get_alerts/info
+nf=/$tp/$alnum-all.msg
+cnf=$tp/$alnum-cve.msg
+descf=$tp/$alnum-desc.msg
+detailf=$tp/$alnum-detailf.msg
+link=${link%$'\r'}
+curl -s $link -o $nf
 egrep "<title>" $nf | egrep -o "[a-Z][0-9]{2,}-[0-9]{2,}-[0-9]{2,}" > $cnf
 egrep "<title>" $nf | cut -d ":" -f2  | sed "s#</title>##g" > $descf
 egrep -i "successful" $nf  > $detailf
-dd=`egrep -o "[0-9]{2} $emf $curyear" 1629-all.msg`
-echo $dd ; echo $alnum 
+d1=`egrep -o "[0-9]{2} $emf $curyear" $nf`
+d2=$(date -d "$d1" +"%d/%m/%Y")
+echo $d2 ; echo $alnum 
 cat $cnf $descf $detailf
 echo
 done
